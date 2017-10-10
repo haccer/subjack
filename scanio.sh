5bf9255fd42d48a101d14027397405c791381b92
haccer <cody@live.se>
2017-10-10 19:39:51 -0400

    Create scanio.sh

---
+++ scanio.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Usage   : ./scanio.sh <version number> <file>
+# Example : ./scanio.sh 20171006 cname_list.txt
+
+# Gathering data from scans.io and parsing it into a file called cname,
+echo "[+] Grabbing CNAME records from scans.io."
+curl -s https://scans.io/data/rapid7/sonar.fdns_v2/$1-fdns.json.gz | \
+  zcat | grep 'type":"cname' | awk -F'":"' '{print $3, $5}' | \
+  awk -F'"' '{print $1, $3}' | sed -e s/" type "/" "/g >> cname_scanio
+
+# List of cnames we're going to grep for.
+declare -a arr=(
+  ".cloudfront.net"
+  ".s3-website"
+  ".s3.amazonaws.com"
+  "-w.amazonaws.com"
+  "-1.amazonaws.com"
+  "-2.amazonaws.com"
+  "s3-external"
+  "s3-accelerate.amazonaws.com"
+  ".herokuapp.com"
+  ".herokussl.com"
+  ".herokudns.com"
+  ".wordpress.com"
+  ".pantheonsite.io"
+  "domains.tumblr.com"
+  ".wpengine.com"
+  ".desk.com"
+  ".zendesk.com"
+  ".github.io"
+  ".global.fastly.net"
+  ".helpjuice.com"
+  ".helpscoutdocs.com"
+  ".ghost.io"
+  "cargocollective.com"
+  "redirect.feedpress.me"
+  ".freshdesk.com"
+  ".myshopify.com"
+  ".statuspage.io"
+  ".uservoice.com"
+  ".surge.sh"
+)
+
+# Grepping cnames from array.
+echo "[+] Sorting CNAME records for interesting content."
+for i in "${arr[@]}"; do
+  grep -F "$i" cname_scanio >> cname_db
+done
+
+# Sorting cname list
+echo "[+] Cleaning up."
+cat cname_db | awk '{print $1}' | sort | uniq >> $2
+
+# Clean up.
+rm cname_db cname_scanio
+echo "[+] Finished."
