10f645d69f487620367f8b8cb39760a528f7213e
haccer <cody@live.se>
2017-10-10 23:04:03 -0400

    Create subjack.go

---
+++ subjack.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+  "bufio"
+  "bytes"
+  "crypto/tls"
+  "fmt"
+  "flag"
+  "io/ioutil"
+  "log"
+  "net/http"
+  "os"
+  "strings"
+  "sync"
+  "syscall"
+  "time"
+)
+
+var (
+  Wordlist = flag.String("w", "", "Path to the wordlist.")
+  Threads  = flag.Int("t", 10, "Number of concurrent threads.")
+  Timeout  = flag.Int("timeout", 10, "Seconds to wait before timeout connection.")
+  Output   = flag.String("o", "", "Output file to write results to.")
+  Https    = flag.Bool("https", false, "Force HTTPS (May increase accuracy. Default: http://).")
+)
+
+type Http struct {
+  Url string
+}
+
+func getDomains(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    log.Fatalln(err)
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+func write(result string) {
+  f, err := os.OpenFile(*Output, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
+  if err != nil {
+    log.Fatalln(err)
+  }
+  defer f.Close()
+
+  if _, err = f.WriteString(result); err != nil {
+    log.Fatalln(err)
+  }
+}
+
+func (s *Http) Get() {
+  var site string
+  if *Https {
+    site = fmt.Sprintf("https://%s", s.Url)
+  } else {
+    site = fmt.Sprintf("http://%s", s.Url)
+  }
+
+  tr := &http.Transport{
+    TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+  }
+  client := &http.Client{
+    Transport: tr,
+    Timeout: time.Duration(*Timeout) * time.Second,
+  }
+  resp, err := client.Get(site)
+  if err != nil {
+    //fmt.Println(err)
+    return
+  }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return
+  }
+
+  var open = fmt.Sprintf("\x1b[1;36m[\x1b[1;31m")
+  var close = fmt.Sprintf("\x1b[1;36m]\x1b[0m")
+
+  var result string
+  if bytes.Contains(body, []byte("ERROR: The request could not be satisfied")) {
+    result = fmt.Sprintf("%sCLOUDFRONT%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("Fastly error: unknown domain")) {
+    result = fmt.Sprintf("%sFASTLY%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("There isn't a GitHub Pages site here.")) {
+    result = fmt.Sprintf("%sGITHUB%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("herokucdn.com/error-pages/no-such-app.html")) {
+    result = fmt.Sprintf("%sHEROKU%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("The gods are wise, but do not know of the site which you seek.")) {
+    result = fmt.Sprintf("%sPANTHEON%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("Whatever you were looking for doesn't currently exist at this address.")) {
+    result = fmt.Sprintf("%sTUMBLR%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("Do you want to register")) {
+    result = fmt.Sprintf("%sWORDPRESS%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("Sorry, We Couldn't Find That Page")) {
+    result = fmt.Sprintf("%sDESK%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("Help Center Closed")) {
+    result = fmt.Sprintf("%sZENDESK%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("Oops - We didn't find your site.")) {
+    result = fmt.Sprintf("%sTEAMWORK%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("We could not find what you're looking for.")) {
+    result = fmt.Sprintf("%sHELPJUICE%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("No settings were found for this company:")) {
+    result = fmt.Sprintf("%sHELPSCOUT%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("The specified bucket does not exist")) {
+    result = fmt.Sprintf("%sS3 BUCKET%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("The thing you were looking for is no longer here, or never was")) {
+    result = fmt.Sprintf("%sGHOST%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("If you're moving your domain away from Cargo you must make this configuration through your registrar's DNS control panel.")) {
+    result = fmt.Sprintf("%sCARGO%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("The feed has not been found.")) {
+    result = fmt.Sprintf("%sFEEDPRESS%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("May be this is still fresh!")) {
+    result = fmt.Sprintf("%sFRESHDESK%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("Sorry, this shop is currently unavailable.")) {
+    result = fmt.Sprintf("%sSHOPIFY%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("You are being <a href=\"https://www.statuspage.io\">redirected")) {
+    result = fmt.Sprintf("%sSTATUSPAGE%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("This domain is successfully pointed at WP Engine, but is not configured for an account on our platform")) {
+    result = fmt.Sprintf("%sWPENGINE%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("This UserVoice subdomain is currently available!")) {
+    result = fmt.Sprintf("%sUSERVOICE%s  %v \n", open, close, s.Url)
+  }
+  if bytes.Contains(body, []byte("project not found")) {
+    result = fmt.Sprintf("%sSURGE%s  %v \n", open, close, s.Url)
+  }
+
+  if strings.ContainsAny(result, "[]") {
+    fmt.Printf(result)
+
+    if *Output != "" {
+      write(result)
+    }
+  }
+}
+
+func magic() {
+  /* For large files */
+  var rLimit syscall.Rlimit
+  err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+  if err != nil {
+    fmt.Println("Error Getting Rlimit ", err)
+  }
+  rLimit.Max = 999999
+  rLimit.Cur = 999999
+  err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+  if err != nil {
+    fmt.Println("Error Setting Rlimit ", err)
+  }
+  err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+  if err != nil {
+    fmt.Println("Error Getting Rlimit ", err)
+  }
+}
+
+func main() {
+  flag.Parse()
+
+  urls := make(chan *Http, *Threads * 10)
+  list, err := getDomains(*Wordlist)
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  if len(list) > 1024 {
+    magic()
+  }
+
+  var wg sync.WaitGroup
+  for i := 0; i < *Threads; i++ {
+    wg.Add(1)
+    go func() {
+      for url := range urls {
+        url.Get()
+      }
+      wg.Done()
+    }()
+  }
+
+  for i := 0; i < len(list); i++ {
+    urls <- &Http{Url: list[i]}
+  }
+  close(urls)
+
+  wg.Wait()
+}
