e1fcbbf097851b320b045030e62d398790c3eb7e
haccer <cody@live.se>
2017-10-11 10:44:59 -0400

    Update cnames.sh

--- cnames.sh
+++ cnames.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# ./cnames.sh example.com
-# Just use awk magic to print only the domain: ./cnames.sh example.com | awk '{print $1}'
+# ./cnames.sh example.txt
+# Just use awk magic to print only the domain: ./cnames.sh example.txt | awk '{print $1}'
 
 for domain in `cat $1`; do
   lookup=$(nslookup $domain | grep -i "canonical name" | awk -F'= ' '{print $2}')
